84d7247a6ef75382ec1c4a4c78b6fe7c8362bf94
Vlad Georgescu <the.vladiator@gmail.com>
2025-01-24 13:12:13 -0800

    changed copyright year to current year

--- staticblog/generator/components/footer.php
+++ staticblog/generator/components/footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-  @Copyright 2025 <?php echo htmlspecialchars($siteName, ENT_QUOTES, 'UTF-8'); ?>
+  @Copyright <?php echo date('Y') . ' ' . htmlspecialchars($siteName, ENT_QUOTES, 'UTF-8'); ?>
 </div>
