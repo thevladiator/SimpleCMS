7937c976a5f797f0273480ff5bd3a4cb2b6be40a
Vlad Georgescu <the.vladiator@gmail.com>
2025-01-27 19:19:56 -0800

    fixed admin home page

--- staticblog/generator/config/Config.php
+++ staticblog/generator/config/Config.php
@@ -1,42 +1,40 @@
 <?php
 
-  class Config {
-    public $PROJECT_ROOT;
-    public $CONTENT_ROOT;
-    public $GENERATOR_ROOT;
-    public $DATABASE_LOCATION;
-    public $SITE_NAME;
-    public $SITE_KEYWORDS;
-    public $SITE_DESCRIPTION;
-    public $SITE_ROOT;
-    public $SITE_URL_ROOT;
+class Config {
+  public $PROJECT_ROOT;
+  public $CONTENT_ROOT;
+  public $GENERATOR_ROOT;
+  public $DATABASE_LOCATION;
+  public $SITE_NAME;
+  public $SITE_KEYWORDS;
+  public $SITE_DESCRIPTION;
+  public $SITE_ROOT;
+  public $SITE_URL_ROOT;
 
-    public function __construct() {
-      $this->PROJECT_ROOT = dirname(dirname(__DIR__));
-      $properties = $this->loadProperties(file: $this->PROJECT_ROOT . '/config/config.properties');
-      $this->CONTENT_ROOT = $this->PROJECT_ROOT . $properties['CONTENT_ROOT'];
-      $this->GENERATOR_ROOT = $this->PROJECT_ROOT . $properties['GENERATOR_ROOT'];
-      $this->DATABASE_LOCATION = $this->PROJECT_ROOT . $properties['CONTENT_DB_LOCATION'];
-      $this->SITE_NAME = $properties['SITE_NAME'];
-      $this->SITE_KEYWORDS = $properties['SITE_KEYWORDS'];
-      $this->SITE_DESCRIPTION = $properties['SITE_DESCRIPTION'];
-      $this->SITE_ROOT = $properties['SITE_ROOT'];
-      $this->SITE_URL_ROOT = $properties['SITE_URL_ROOT'];
+  public function __construct() {
+    $this->PROJECT_ROOT = dirname(dirname(__DIR__));
+    $properties = $this->loadProperties(file: $this->PROJECT_ROOT . '/config/config.properties');
+    $this->CONTENT_ROOT = $this->PROJECT_ROOT . $properties['CONTENT_ROOT'];
+    $this->GENERATOR_ROOT = $this->PROJECT_ROOT . $properties['GENERATOR_ROOT'];
+    $this->DATABASE_LOCATION = $this->PROJECT_ROOT . $properties['CONTENT_DB_LOCATION'];
+    $this->SITE_NAME = $properties['SITE_NAME'];
+    $this->SITE_KEYWORDS = $properties['SITE_KEYWORDS'];
+    $this->SITE_DESCRIPTION = $properties['SITE_DESCRIPTION'];
+    $this->SITE_ROOT = $properties['SITE_ROOT'];
+    $this->SITE_URL_ROOT = $properties['SITE_URL_ROOT'];
+  }
+
+  private function loadProperties($file) {
+    $properties = [];
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        // Skip comments
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+        list($key, $value) = explode('=', $line, 2);
+        $properties[trim($key)] = trim($value);
     }
-
-    private function loadProperties($file) {
-      $properties = [];
-      $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-      foreach ($lines as $line) {
-          // Skip comments
-          if (strpos(trim($line), '#') === 0) {
-              continue;
-          }
-          list($key, $value) = explode('=', $line, 2);
-          $properties[trim($key)] = trim($value);
-      }
-      return $properties;
+    return $properties;
   }
-  }
-
-?>
+}
